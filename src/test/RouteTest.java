02cebc15b5e7c6f206d2bdff5d8c646b23f8cac6
ppiedel <zespolok666>
2017-10-09 09:53:42 +0200

    Route getTotalDistance tests finished.

--- src/test/RouteTest.java
+++ src/test/RouteTest.java
@@ -1,7 +1,13 @@
 package test;
 
 import main.City;
+import main.Route;
 import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 /**
@@ -14,8 +20,22 @@ public class RouteTest {
         City city1 = new City(0,0);
         City city2 = new City(10,10);
         City city3 = new City(20,20);
+        Route route = new Route(Arrays.asList(city1,city2,city3));
 
         double distance1 = city1.distanceTo(city2);
+        double distace2 = city2.distanceTo(city3);
+        double distance3 = city3.distanceTo(city1);
+
+        assertEquals(distance1+distace2+distance3,route.getTotalDistance());
+
+    }
+
+    @Test
+    public void getTotalDistanceShouldReturnZero() throws Exception {
+        City city = new City(100,100);
+        Route route = new Route(Collections.singletonList(city));
+
+        assertEquals(0,route.getTotalDistance());
     }
 
 }
